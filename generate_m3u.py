cb273142c88e02e412a5b2cfd2db9dc3be32c335
dave <dave.berenbaum@gmail.com>
2020-12-20 20:23:37 -0500

    save to mounted drive

--- generate_m3u.py
+++ generate_m3u.py
@@ -7,15 +7,20 @@ from bs4 import BeautifulSoup
 
 date = input('Date (YYYY-MM-DD):')
 
-r = requests.get('http://www.phishtracks.com/shows/%s' % (date,))
-html = r.text
+url = 'http://phishtracks.com/shows/%s' % date
+resp = requests.get(url)
+html = resp.text
 soup = BeautifulSoup(html, 'html.parser')
 script = soup.find('script', text=re.compile('PTData'))
 
-data = re.findall('PTData[\s\n]*=[\s\n]*\{[^;]+;', script.text)[0]
+for line in script.text.split("\n"):
+    line = line.strip()
+    if line.startswith("PTData"):
+        data = line
+        break
 
 data = re.sub('\n', '', data)
-data = re.sub('[^{]*', '', data, count=1)
+data = re.sub('PTData\s*=\s*', '', data, count=1)
 data = re.sub(';.*', '', data, count=1)
 
 js_data = json.loads(data)
@@ -28,5 +33,5 @@ for show_set in js_data['sets']:
         m3u += '#EXTINF:%d,Phish - %s\n' % (track['duration']/1000, track['title'])
         m3u += track['file_url'] + '\n'
 
-f = open('phish_%s.m3u' % (date,), 'w')
+f = open('/mnt/playlists/phish_%s.m3u' % (date,), 'w')
 f.write(m3u)
